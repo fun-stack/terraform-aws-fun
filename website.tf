25aa6efd26ceccb0749ad9d676897f6f5629aec6
johannes karoff <johannes@karoff.net>
2021-05-05 20:56:17 +0200

    compress by default

--- website.tf
+++ website.tf
@@ -77,6 +77,7 @@ resource "aws_cloudfront_distribution" "website" {
       }
     }
 
+    compress               = true
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
