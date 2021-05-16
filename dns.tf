f629872d2659f066f2075232cd633c55087eceb9
johannes karoff <johannes@karoff.net>
2021-05-17 00:45:42 +0200

    www redirect

--- dns.tf
+++ dns.tf
@@ -8,7 +8,7 @@ resource "aws_acm_certificate" "website" {
   provider          = aws.us
 }
 
-resource "aws_route53_record" "certificate_validation" {
+resource "aws_route53_record" "certificate_validation_website" {
   for_each = {
     for dvo in aws_acm_certificate.website.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -28,7 +28,38 @@ resource "aws_route53_record" "certificate_validation" {
 resource "aws_acm_certificate_validation" "website" {
   certificate_arn = aws_acm_certificate.website.arn
   validation_record_fqdns = [
-    for dvo in aws_acm_certificate.website.domain_validation_options : aws_route53_record.certificate_validation[dvo.domain_name].fqdn
+    for dvo in aws_acm_certificate.website.domain_validation_options : aws_route53_record.certificate_validation_website[dvo.domain_name].fqdn
+  ]
+  provider = aws.us
+}
+
+resource "aws_acm_certificate" "website_www" {
+  domain_name       = "www.${local.domain_website}"
+  validation_method = "DNS"
+  provider          = aws.us
+}
+
+resource "aws_route53_record" "certificate_validation_website_www" {
+  for_each = {
+    for dvo in aws_acm_certificate.website_www.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.domain.zone_id
+}
+
+resource "aws_acm_certificate_validation" "website_www" {
+  certificate_arn = aws_acm_certificate.website_www.arn
+  validation_record_fqdns = [
+    for dvo in aws_acm_certificate.website_www.domain_validation_options : aws_route53_record.certificate_validation_website_www[dvo.domain_name].fqdn
   ]
   provider = aws.us
 }
---
+++ website_redirect.tf
@@ -0,0 +1,74 @@
+resource "aws_s3_bucket" "website_www" {
+  bucket_prefix = "${local.prefix}-website-www"
+  acl           = "public-read"
+
+  website {
+    redirect_all_requests_to = "https://${local.domain_website}"
+  }
+}
+
+resource "aws_cloudfront_distribution" "website_www" {
+  origin {
+    domain_name = aws_s3_bucket.website_www.website_endpoint
+    origin_id   = aws_s3_bucket.website_www.id
+
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = ["www.${local.domain_website}"]
+
+  #TODO
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.website_www.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.website_www.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  wait_for_deployment = true
+}
+
+resource "aws_route53_record" "website_www" {
+  name    = "www.${local.domain_website}"
+  type    = "A"
+  zone_id = data.aws_route53_zone.domain.zone_id
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.website_www.domain_name
+    zone_id                = aws_cloudfront_distribution.website_www.hosted_zone_id
+  }
+}
