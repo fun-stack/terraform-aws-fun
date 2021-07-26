8c45da52a50a6b554d84d83fa14315e8313267ad
johannes karoff <johannes@karoff.net>
2021-07-26 11:42:39 +0200

    add http role and prefix output

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "api_role" {
   value = concat(module.api[*].api_role, [null])[0]
 }
+
+output "http_role" {
+  value = concat(module.http[*].http_role, [null])[0]
+}
+
+output "prefix" {
+  value = local.prefix
+}
