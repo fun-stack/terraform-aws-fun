a7dfa828dcbd11181f9de51cef58894789f6e744
johannes karoff <johannes@karoff.net>
2021-08-01 14:14:34 +0200

    add domains and hosted zone to output

--- output.tf
+++ output.tf
@@ -9,3 +9,23 @@ output "http_role" {
 output "prefix" {
   value = local.prefix
 }
+
+output "domain_website" {
+  value = local.domain_website
+}
+
+output "domain_http" {
+  value = local.domain_http
+}
+
+output "domain_ws" {
+  value = local.domain_ws
+}
+
+output "domain_auth" {
+  value = local.domain_auth
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.domain.zone_id
+}
