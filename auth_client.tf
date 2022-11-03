6b3425d23b2f3e7337d6c2844b9c91661114366c
GRBurst <GRBurst@protonmail.com>
2022-11-03 17:00:15 +0100

    replace local.x with var.x on modules with defaults
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- auth_client.tf
+++ auth_client.tf
@@ -1,16 +1,16 @@
 module "auth_client" {
-  count  = local.auth == null ? 0 : 1
+  count  = var.auth == null ? 0 : 1
   source = "./auth_client"
 
   prefix                = local.prefix
 
-  css_content          = local.auth.css_content
-  image_base64_content = local.auth.image_base64_content
+  css_content          = var.auth.css_content
+  image_base64_content = var.auth.image_base64_content
 
   user_pool_id = module.auth[0].user_pool.id
   api_scopes   = module.auth[0].api_scopes
 
-  redirect_urls = local.auth_redirect_urls
+  redirect_urls = var.auth_redirect_urls
 
   providers = {
     aws           = aws
