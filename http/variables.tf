e87a3e0981dc7e60ec51edc77b121c9ae8a87a2e
johannes karoff <johannes@karoff.net>
2023-05-11 12:20:08 +0200

    fix missing layers variable

--- http/variables.tf
+++ http/variables.tf
@@ -31,6 +31,7 @@ variable "api" {
     timeout       = number
     memory_size   = number
     environment   = optional(map(string))
+    layers        = optional(list(string))
     vpc_config = optional(object({
       subnet_ids         = list(string)
       security_group_ids = list(string)
@@ -47,6 +48,7 @@ variable "rpc" {
     timeout       = number
     memory_size   = number
     environment   = optional(map(string))
+    layers        = optional(list(string))
     vpc_config = optional(object({
       subnet_ids         = list(string)
       security_group_ids = list(string)
